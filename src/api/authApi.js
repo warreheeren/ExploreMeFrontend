72b10c0a52e0192b2776e2bf37b4e7dc40de0b57
Warre Heeren <65282396+warreheeren@users.noreply.github.com>
2026-05-04 16:27:13 +0200

    Update API endpoints in authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,11 +2,11 @@ import http from './http'
 import { AuthUser } from '@/models'
 
 export async function register(payload) {
-  const { data } = await http.post('/auth/register', payload)
+  const { data } = await http.post('api/auth/register', payload)
   return AuthUser.fromApi(data)
 }
 
 export async function login(payload) {
-  const { data } = await http.post('/auth/login', payload)
+  const { data } = await http.post('api/auth/login', payload)
   return AuthUser.fromApi(data)
 }
